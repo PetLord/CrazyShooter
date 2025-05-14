12a3dd8c7e673054b00a3674e05a16a41f4c6dc9
PetLord <dkrisztian54@gmail.com>
2025-05-14 15:28:02 +0300

    Added menu

--- CrazyShooter/Program.cs
+++ CrazyShooter/Program.cs
@@ -3,15 +3,32 @@ using CrazyShooter.Tools;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 using CrazyShooter.Scene;
+using CrazyShooter.UI;
+using Silk.NET.Input;
 
 namespace CrazyShooter;
 
+// todo! fix font style
+
 class Program
 {
+    private enum GameState
+    {
+        MainMenu,
+        Playing,
+        Paused
+    }
+
     private static IWindow graphicWindow;
+    private static IInputContext inputContext;
+    
     private static GL gl;
     private static uint program;
+
+    private static GameState gameState = GameState.MainMenu;
     private static Scene.Scene? currentScene;
+    private static Menu menu;
+    
     static void Main()
     {
         WindowOptions windowOptions = WindowOptions.Default;
@@ -27,6 +44,7 @@ class Program
         graphicWindow.FramebufferResize += size =>
         {
             gl.Viewport(size);
+            menu.WindowResized(size);
             if (currentScene is not null)
             {
                 currentScene.AspectRatio = (float)size.X / size.Y;
@@ -40,6 +58,8 @@ class Program
     {
         gl = graphicWindow.CreateOpenGL();
         program = ProgramUtils.LinkProgram(gl);
+        inputContext = graphicWindow.CreateInput();
+        menu = new Menu(gl, graphicWindow, inputContext);
         gl.Enable(GLEnum.DepthTest);
         gl.DepthFunc(DepthFunction.Lequal);
         gl.ClearColor(0.1f, 0.1f, 0.1f, 1f);
@@ -55,17 +75,67 @@ class Program
     
     private static void GraphicWindow_Update(double deltaTime)
     {
-        if (currentScene is not null)
+        switch (gameState)
         {
-            currentScene.Update(deltaTime);
+            case GameState.Playing:
+                break;
+
+            case GameState.MainMenu:
+                menu.Update(deltaTime);
+                break;
+            
+            case GameState.Paused:
+                break;
         }
     }
 
     private static unsafe void GraphicWindow_Render(double deltaTime)
+    {
+        switch (gameState)
+        {
+            case GameState.Playing:
+                RenderPlaying();
+                break;
+
+            case GameState.MainMenu:
+                RenderMenu();
+                break;
+            
+            case GameState.Paused:
+                RenderPaused();
+                break;
+        }
+    }
+
+    private static unsafe void RenderPlaying()
     {
         if (currentScene is not null)
         {
             currentScene.Render(gl);
         }
     }
+
+    private static unsafe void RenderMenu()
+    {
+        if (menu is not null)
+        {
+            menu.Render();
+        }
+    }
+
+    private static unsafe void RenderPaused()
+    {
+        // to do
+    }
+
+    public static void StartGame()
+    {
+        gameState = GameState.Playing;
+    }
+    
+    public static void ExitGame()
+    {
+        System.Environment.Exit(0);
+    }
+    
 }
